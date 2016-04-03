ee2d5c9de03689f5ba42f82023ec1b113f09622d
hwanginsitein <hwanginsitein@126.com>
2016-04-03 15:07:36 +0800

    first

---
+++ protected/modules/admin/views/user/_form.php
@@ -0,0 +1,63 @@
+<?php
+/* @var $this UserController */
+/* @var $model User */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'user-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->textField($model,'username',array('size'=>21,'maxlength'=>21)); ?>
+		<?php echo $form->error($model,'username'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'password'); ?>
+                <?php $model->password = "";?>
+		<?php echo $form->passwordField($model,'password',array('size'=>30,'maxlength'=>30)); ?>
+		<?php echo $form->error($model,'password'); ?>
+	</div>
+	<div class="row">
+		<?php echo $form->labelEx($model,'phone'); ?>
+		<?php echo $form->textField($model,'phone',array('size'=>20,'maxlength'=>20)); ?>
+		<?php echo $form->error($model,'phone'); ?>
+	</div>
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>32,'maxlength'=>32)); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'approvement'); ?>
+		<?php echo $form->checkbox($model,'approvement'); ?>
+		<?php echo $form->error($model,'approvement'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'permission'); ?>
+		<?php echo $form->textField($model,'permission',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'permission'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
