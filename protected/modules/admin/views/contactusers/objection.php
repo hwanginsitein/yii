de9c616e06db869cf6808bcf5415412b6f44ae6c
hwanginsitein <hwanginsitein@126.com>
2016-05-22 16:33:49 +0800

    first

--- protected/modules/admin/views/contactusers/objection.php
+++ protected/modules/admin/views/contactusers/objection.php
@@ -1,1 +1,24 @@
-
+<div id="objection_content">
+    <div>
+        <span>提出异议的用户</span>
+        <span><button id="allObjectionusers">显示所有的异议用户</button></span>
+        <span><button id="validObjectionusers">显示异议成立用户</button></span>
+    </div>
+    <div id="objections">
+        
+    </div>
+</div>
+<script>
+    $("#allObjectionusers").click(function(){
+        $.ajax({
+            type: "POST",
+            url: "/admin/contactusers/getObjections",
+            success:function(data){
+                $("#objections").html(data);
+            }
+        })
+    });
+    $("#validObjectionusers").click(function(){
+        
+    });
+</script>
